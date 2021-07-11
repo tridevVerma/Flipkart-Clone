7400cd4ea61922d5b45a880b36192b29fda5226b
Tridev Verma <tv072000@gmail.com>
2021-07-12 00:04:32 +0530

    changed server.js and package.json

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ app.use(express.json());
 app.use("/newUser", registerRoute);
 app.use("/login", loginRoute);
 app.use("/products", productsRoute);
+
 const URL = process.env.MONGODB_URI || process.env.DB_URL;
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
