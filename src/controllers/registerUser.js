7010ac3806e2e4cb5420926b801269c99d8a3f03
Tridev Verma <tv072000@gmail.com>
2021-07-14 20:52:07 +0530

    email or phone no validation for new User added

--- src/controllers/registerUser.js
+++ src/controllers/registerUser.js
@@ -2,17 +2,14 @@ import User from "../models/newUserSchema.js";
 
 const registerUser = async (req, res) => {
   try {
-    let mobileExist, emailExist;
-    if (Number(req.body.emailORPhone))
-      mobileExist = await User.findOne({
-        mobileno: Number(req.body.emailORPhone),
-      });
-    else emailExist = await User.findOne({ email: req.body.emailORPhone });
+    const mobileExist = await User.findOne({ mobileno: req.body.mobileno });
+    const emailExist = await User.findOne({ email: req.body.email });
 
     if (mobileExist || emailExist) {
       console.log("User exists");
       return res.status(401).json("User already exists");
     }
+
     const newUser = await new User(req.body);
     const result = await newUser.save();
     res.status(201).json("User is successfully registered");
